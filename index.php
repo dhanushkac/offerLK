35413cce7a3b754a0e478fe79c758e9e757ac5c7
Dhanushka Chandana <dhanushka787@gmail.com>
2016-02-20 21:26:34 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>OfferLK</title>
+
+    <!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" class="index">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#page-top">OfferLK</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#">Home</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#">Categories</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#">About</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <!-- main container-->
+    <div class="main-block">
+        <div class="row main-row">
+            <!-- left side bar -->
+            <div class="col-lg-3 left-block">
+                <!-- ending soon -->
+                <p class="fragment-title">Ending Soon</p>
+                <div class="offer-fragment-block">
+                    <div class="offer-fragment">
+                        <p class="offer-fragment-title">Sample offer title goes here </p>
+                        <a href="#" class="link-normal">view more</a>
+
+                    </div>
+                    <div class="offer-fragment-bottom">
+                        32 out of 50
+                    </div>
+                </div>
+
+                <div class="offer-fragment-block">
+                    <div class="offer-fragment">
+                        <p class="offer-fragment-title">Sample offer title goes here </p>
+                        <a href="#" class="link-normal">view more</a>
+
+                    </div>
+                    <div class="offer-fragment-bottom">
+                        32 out of 50
+                    </div>
+                </div>
+
+                <div class="offer-fragment-block">
+                    <div class="offer-fragment">
+                        <p class="offer-fragment-title">Sample offer title goes here </p>
+                        <a href="#" class="link-normal">view more</a>
+
+                    </div>
+                    <div class="offer-fragment-bottom">
+                        32 out of 50
+                    </div>
+                </div>
+            </div>
+            <!-- content -->
+            <div class="col-lg-6 middle-content">
+                <div class="offers">
+                    <div class="offer">
+                        <div class="row offer-row">
+                            <!-- offer description -->
+                            <div class="col-lg-10">
+                                <div class="offer-middle">
+                                    <a class="offer-title" href="#">Sample offer title goes here
+                                    </a>
+                                </div>
+                                <div class="offer-middle offer-desc">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum
+                                        arcu augue, non convallis sem euismod ut.. <a href="#" class="link-small">view more</a> </p>
+                                </div>
+                                <div class="offer-btn offer-middle">
+                                    <a href="#" class="btn btn-sm btn-outline">
+                                        <i class="fa fa-plus"></i> Join this offer
+                                    </a>
+                                </div>
+                            </div>
+                            <!-- offer joined -->
+                            <div class="col-lg-2 offer-joined offer-end">
+                                <div class="">
+                                    <p class="offer-count-head">92</p>
+                                    <p class="offer-out-of">out of</p>
+                                    <p class="offer-count-bottom">100</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="offer">
+                        <div class="row offer-row">
+                            <!-- offer description -->
+                            <div class="col-lg-10">
+                                <div class="offer-middle">
+                                    <a class="offer-title" href="#">Sample offer title goes here
+                                    </a>
+                                </div>
+                                <div class="offer-middle offer-desc">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum
+                                        arcu augue, non convallis sem euismod ut..<a href="#" class="link-small">view more</a></p>
+                                </div>
+                                <div class="offer-btn offer-middle">
+                                    <a href="#" class="btn btn-sm btn-outline">
+                                        <i class="fa fa-plus"></i> Join this offer
+                                    </a>
+                                </div>
+                            </div>
+                            <!-- offer joined -->
+                            <div class="col-lg-2 offer-joined offer-begin">
+                                <div class="">
+                                    <p class="offer-count-head">2</p>
+                                    <p class="offer-out-of">out of</p>
+                                    <p class="offer-count-bottom">10</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="offer">
+                        <div class="row offer-row">
+                            <!-- offer description -->
+                            <div class="col-lg-10">
+                                <div class="offer-middle">
+                                    <a class="offer-title" href="#">Sample offer title goes here
+                                    </a>
+                                </div>
+                                <div class="offer-middle offer-desc">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum
+                                        arcu augue, non convallis sem euismod ut..<a href="#" class="link-small">view more</a></p>
+                                </div>
+                                <div class="offer-btn offer-middle">
+                                    <a href="#" class="btn btn-sm btn-outline">
+                                        <i class="fa fa-plus"></i> Join this offer
+                                    </a>
+                                </div>
+                            </div>
+                            <!-- offer joined -->
+                            <div class="col-lg-2 offer-joined offer-begin">
+                                <div class="">
+                                    <p class="offer-count-head">2</p>
+                                    <p class="offer-out-of">out of</p>
+                                    <p class="offer-count-bottom">10</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="offer">
+                        <div class="row offer-row">
+                            <!-- offer description -->
+                            <div class="col-lg-10">
+                                <div class="offer-middle">
+                                    <a class="offer-title" href="#">Sample offer title goes here
+                                    </a>
+                                </div>
+                                <div class="offer-middle offer-desc">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum
+                                        arcu augue, non convallis sem euismod ut..<a href="#" class="link-small">view more</a></p>
+                                </div>
+                                <div class="offer-btn offer-middle">
+                                    <a href="#" class="btn btn-sm btn-outline">
+                                        <i class="fa fa-plus"></i> Join this offer
+                                    </a>
+                                </div>
+                            </div>
+                            <!-- offer joined -->
+                            <div class="col-lg-2 offer-joined offer-begin">
+                                <div class="">
+                                    <p class="offer-count-head">2</p>
+                                    <p class="offer-out-of">out of</p>
+                                    <p class="offer-count-bottom">10</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="offer">
+                        <div class="row offer-row">
+                            <!-- offer description -->
+                            <div class="col-lg-10">
+                                <div class="offer-middle">
+                                    <a class="offer-title" href="#">Sample offer title goes here
+                                    </a>
+                                </div>
+                                <div class="offer-middle offer-desc">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum
+                                        arcu augue, non convallis sem euismod ut..<a href="#" class="link-small">view more</a></p>
+                                </div>
+                                <div class="offer-btn offer-middle">
+                                    <a href="#" class="btn btn-sm btn-outline">
+                                        <i class="fa fa-plus"></i> Join this offer
+                                    </a>
+                                </div>
+                            </div>
+                            <!-- offer joined -->
+                            <div class="col-lg-2 offer-joined offer-end">
+                                <div class="">
+                                    <p class="offer-count-head">92</p>
+                                    <p class="offer-out-of">out of</p>
+                                    <p class="offer-count-bottom">100</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- right side bar -->
+            <div class="col-lg-3 right-block">
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <footer class="text-center">
+        <div class="footer-below">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        Copyright &copy; OfferLK 2016
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-top page-scroll visible-xs visible-sm">
+        <a class="btn btn-primary" href="#page-top">
+            <i class="fa fa-chevron-up"></i>
+        </a>
+    </div>
+
+    </div>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/cbpAnimatedHeader.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/script.js"></script>
+
+</body>
+
+</html>
